a5ccd23f38d03d5582cfedbc9b9dad1fafd5d6ba
dungkhaitran <tran0916@uw.edu>
2021-02-16 15:27:54 -0800

    added far range attack

--- boundingbox.js
+++ boundingbox.js
@@ -12,17 +12,4 @@ class BoundingBox {
         if (this.right > oth.left && this.left < oth.right && this.top < oth.bottom && this.bottom > oth.top) return true;
         return false;
     };
-
-    // VBool VObject::IsCollision( VGameObjectPtr pObject )
-    // {	
-    //     if (pObject)
-    //     {
-    //         if (this == pObject || (this->m_rCollisionBound.topLeft.nX >= pObject->m_rCollisionBound.bottomRight.nX)
-    //             || (this->m_rCollisionBound.topLeft.nY >= pObject->m_rCollisionBound.bottomRight.nY)
-    //             || (this->m_rCollisionBound.bottomRight.nX <= pObject->m_rCollisionBound.topLeft.nX)
-    //             || (this->m_rCollisionBound.bottomRight.nY <= pObject->m_rCollisionBound.topLeft.nY))
-    //             return VFalse;
-    //     }
-    //     return VTrue;
-    // }
 };
