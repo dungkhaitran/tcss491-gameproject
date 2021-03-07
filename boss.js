e15c44f31f84c259b7bff93f0c049c9867b95463
qp1993 <52257087+qp1993@users.noreply.github.com>
2021-03-07 00:24:05 -0800

    config boss

--- boss.js
+++ boss.js
@@ -1,12 +1,12 @@
-class Boss {
-    constructor(game, x, y){
+class Boss extends RangeEnemies {
+    constructor(game, x, y) {
+        super(game, x, y);
         Object.assign(this, { game, x, y });
 
-        this.velocityX = PARAMS.BITWIDTH / 15;
+        this.velocityX = PARAMS.BITWIDTH / 5;
         this.velocity = { x: -this.velocityX, y: 0 }; // pixels per second
-        // this.velocity = { x: -PARAMS.BITWIDTH, y: 0}; // 16 pixels per second
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/boss/boss.png");
-
+        // this.state = STATE.MOVING;
         this.facing = FACING_SIDE.LEFT;
 
         this.paused = true;
@@ -14,13 +14,13 @@ class Boss {
         this.deadCounter = 0;
         this.flickerFlag = true;
 
-        this.width = 48;
+        this.width = 96;
         this.height = 96;
 
         this.animations = [];        
         this.loadAnimations();
 
-        this.hp = 10000;
+        this.hp = 1000;
         this.maxHp = this.hp;
         this.farDamage = 500;
 
@@ -31,21 +31,11 @@ class Boss {
         this.farAttackCooldown = 0;
 
         this.canAttackFar = true;
-        this.farAttackRangeWidth = 600;
+        this.farAttackRangeWidth = 300;
 
         this.beingDamaged = false;
 
         this.updateBB();
-
-        this.BB = new BoundingBox(this.x, this.y, this.width, this.height);
-        this.BBFarAttackRange = new BoundingBox(this.x + this.width, this.y, this.farAttackRangeWidth, PARAMS.CANVAS_WIDTH);
-
-        this.gotDamaged = false;
-        this.gotDamaged2 = false;
-        
-        this.dealDamage = false;
-
-        this.healthBar = new HealthBar(this, game);
     }
 
     loadAnimations() {
@@ -57,113 +47,73 @@ class Boss {
         }
 
         // facing left
-        this.animations[STATE.IDLE][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 1300, 1355, 85, 122, 3, 0.2, 173, true, true); // idle
-        this.animations[STATE.MOVING][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 529, 582, 86, 122, 6, 0.15, 173, true, true);  // run
+        this.animations[STATE.IDLE][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 1255, 1310, 145, 160, 3, 0.2, 110, true, true); // idle
+        this.animations[STATE.MOVING][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 500, 540, 145, 160, 6, 0.15, 110, true, true);  // run
 
-        this.animations[STATE.ATTACKING][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 235, 1567, 108, 160, 7, 0.15, 106, true, true);  // melee
+        this.animations[STATE.ATTACKING][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 170, 1567, 210, 160, 7, 0.15, 45, true, true);  // melee
         // this.animations[STATE.ATTACKING][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 728, 1850, 135, 133, 5, 0.15, 106, true, true);  // fire
         // this.animations[STATE.ATTACKING][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 760, 2119, 142, 122, 5, 0.15, 106, true, true);  // ice
         // this.animations[STATE.ATTACKING][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 775, 295, 108, 152, 5, 0.15, 148, true, true);  // thunder
 
-        this.animations[STATE.HIT][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 1548, 1100, 103, 116, 2, 0.1, 152, true, true);  // hit
-        this.animations[STATE.DEAD][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 155, 74, 160, 119, 6, 0.15, 164, true, false); // dead
+        this.animations[STATE.HIT][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 1500, 1050, 145, 160, 2, 0.1, 152, true, true);  // hit
+        this.animations[STATE.DEAD][FACING_SIDE.LEFT] = new Animator(this.spritesheet, 140, 30, 200, 160, 6, 0.15, 120, true, false); // dead
         
         // facing right
-        this.animations[STATE.IDLE][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2000, 1355, 85, 122, 3, 0.2, 173, false, true); // idle
-        this.animations[STATE.MOVING][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2000, 582, 86, 122, 6, 0.15, 173, false, true);  // run
+        this.animations[STATE.IDLE][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2005, 1310, 145, 160, 3, 0.2, 110, false, true); // idle
+        this.animations[STATE.MOVING][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2000, 540, 145, 160, 6, 0.15, 110, false, true);  // run
 
-        this.animations[STATE.ATTACKING][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2005, 1567, 108, 160, 7, 0.15, 106, false, true);  // melee
+        this.animations[STATE.ATTACKING][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 1990, 1567, 210, 160, 7, 0.15, 45, false, true);  // melee
         // this.animations[STATE.ATTACKING][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2003, 1850, 135, 133, 5, 0.15, 106, false, true);  // fire
         // this.animations[STATE.ATTACKING][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 1990, 2119, 142, 122, 5, 0.15, 106, false, true);  // ice
         // this.animations[STATE.ATTACKING][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2000, 295, 108, 152, 5, 0.15, 148, false, true);  // thunder
 
-        this.animations[STATE.HIT][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 1994, 1100, 103, 116, 2, 0.1, 152, false, true);  // hit
-        this.animations[STATE.DEAD][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 2028, 74, 160, 119, 6, 0.15, 164, false, false); // dead
+        this.animations[STATE.HIT][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 1950, 1050, 145, 160, 2, 0.1, 152, false, true);  // hit
+        this.animations[STATE.DEAD][FACING_SIDE.RIGHT] = new Animator(this.spritesheet, 1980, 30, 200, 160, 6, 0.15, 120, false, false); // dead
 
     }
 
-    update(){
+    update() {
+        if (super.update()) {
+            var bullet = null
 
-        const TICK = this.game.clockTick;
-
-        // var bullet = null
-            
-        // if (this.facing === FACING_SIDE.RIGHT) {
-        //     bullet = new FireOfCultist(this.game, this.BB.x + this.BB.width, this.BB.y + (this.BB.height - 48) / 3)
-        //     bullet.velocity.x = bullet.velocityX
-        // } else {
-        //     bullet = new FireOfCultist(this.game, this.BB.x - this.BB.width, this.BB.y + (this.BB.height - 48) / 3)
-        //     bullet.velocity.x = -bullet.velocityX
-        // }
-        // bullet.facing = this.facing;
-        // bullet.farDamage = this.farDamage;
-        // setTimeout(() => {this.game.addEntity(bullet);}, 600);
-
-        // this.updateBB();
-
-        if (this.dead){
-            this.state = STATE.DEAD;
-            this.deadCounter += this.game.clockTick;
-            if(this.deadCounter > 2.5) this.removeFromWorld = true;
-        }
-
-        if (this.farAttackCooldown > 0) {
-            this.farAttackCooldown -= TICK;
-            if (this.farAttackCooldown <= 0) {
-                this.canAttackFar = true;
+            if (this.facing === FACING_SIDE.RIGHT) {
+                bullet = new FireOfCultist(this.game, this.BB.x + this.BB.width, this.BB.y + (this.BB.height - 48) / 1.5)
+                bullet.velocity.x = bullet.velocityX
+            } else {
+                bullet = new FireOfCultist(this.game, this.BB.x - this.BB.width,  this.BB.y + (this.BB.height - 48) / 1.5)
+                bullet.velocity.x = -bullet.velocityX
             }
-        }
-    
-        var createBullet = false
-        if (this.attacking) {
-            if (this.farAttackDuration > 0) {
-                this.farAttackDuration -= TICK;
-                if (this.farAttackDuration <= 0) {
-                    this.state = STATE.IDLE;
-                    this.attacking = false;
-                    this.dealDamage = false;
-                }
-            }
-            if (this.farAttackDuration <= 0 && this.farAttackCooldown <= 0) {
-                this.canAttackFar = false;
-                this.state = STATE.ATTACKING;
-                this.farAttackDuration = this.FAR_ATTACK_DURATION;
-                this.farAttackCooldown = this.FAR_ATTACK_COOLDOWN;
-                createBullet = true
-            }
+            bullet.facing = this.facing
+            bullet.farDamage = this.farDamage;
+            this.game.addEntity(bullet)
         }
 
-        if(this.hp <= 0){
-            this.dead = true;
-        }
-
-        return createBullet
+        this.x += this.velocity.x;
+        this.updateBB();
     }
 
     updateBB() {
         if(this.facing == FACING_SIDE.RIGHT){
-            this.BB = new BoundingBox(this.x + 10, this.y + 10, this.width * 1.5, this.height + 10); // body
-            this.BBFarAttackRange = new BoundingBox(this.BB.x, this.BB.y, 
+            this.BB = new BoundingBox(this.x, this.y + this.height + 10, this.width * 2, this.height * 1.5); // body
+            this.BBFarAttackRange = new BoundingBox(this.BB.x + this.width*2, this.BB.y, 
                 this.farAttackRangeWidth, this.BB.height); // range attack
         } else {
-            this.BB = new BoundingBox(this.x + 30, this.y + 15, this.width * 1.5, this.height + 10);
+            this.BB = new BoundingBox(this.x + this.width, this.y + this.height + 10, this.width * 2, this.height * 1.5);
             this.BBFarAttackRange = new BoundingBox(this.BB.x - this.farAttackRangeWidth, this.BB.y,
                 this.farAttackRangeWidth, this.BB.height);
         }
     };
 
+    drawMinimap(ctx, mmX, mmY){
+
+    }
+    
     draw(ctx) {
-        
-        this.healthBar.draw(ctx);
-        //this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 3);
-
-        if (PARAMS.DEBUG) {
-            ctx.strokeStyle = 'Red';
-            ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y, this.BB.width, this.BB.height);
-
-            ctx.strokeStyle = 'Blue';
-            ctx.strokeRect(this.BBFarAttackRange.x - this.game.camera.x, this.BBFarAttackRange.y,
-                this.BBFarAttackRange.width, this.BBFarAttackRange.height);
+        if(this.state === STATE.DEAD && this.facing === FACING_SIDE.LEFT){
+            this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 2);
+        }else{
+            this.animations[this.state][this.facing].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y, 2);
         }
+        super.draw(ctx);
     }
 }
